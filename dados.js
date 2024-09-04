057424cad74c0c5f39613f7ee2e083c5c6502996
nezabia <beatriz.blima3@gmail.com>
2024-09-04 13:00:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [ //let define uma variável. aqui o nome da variável é dados e nela vou armazenar o nome dos produtos
+    {
+        titulo : "Ração Golden", 
+        descricao : "Buscando uma ração Premium de ótima qualidade para nutrir seu cãozinho? Conheça a ração Golden Special. Com o delicioso sabor de frango e carne, será um verdadeiro banquete a ser oferecido ao seu pet.",
+        link : "https://www.petz.com.br/produto/racao-golden-special-para-caes-adultos-sabor-frango-e-carne-71343"
+    }, 
+    {
+        titulo : "Areia para Gatos", 
+        descricao : "Os tutores de felinos já sabem que, na hora de fazer suas necessidades, os bichanos procuram lugares com areia para poder enterrar seus dejetos. Sendo assim, pensando no bem-estar e comodidade desses pets, a Kelco Pet desenvolveu a Pipicat 12 kg!",
+        link : "https://www.petz.com.br/produto/areia-higienica-cansei-de-ser-gato-biodegradavel-graos-finos-184527"
+    }, 
+    {
+        titulo : "Alimento Supra Funny Bunny", 
+        descricao : "Buscando uma ração Premium de ótima qualidade para nutrir seu cãozinho? Conheça a ração Golden Special. Com o delicioso sabor de frango e carne, será um verdadeiro banquete a ser oferecido ao seu pet.",
+        link : "https://www.petz.com.br/produto/alimento-supra-funny-bunny-delicias-da-horta-para-roedores-80397"
+    },
+];
+
