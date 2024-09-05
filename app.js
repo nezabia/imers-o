8c9fe73a683c9e0eadaa73baab711c26eb9066d7
nezabia <beatriz.blima3@gmail.com>
2024-09-05 12:05:57 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log(dados);
+
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+    let resultados = ""
+
+for (let dado of dados) { //para cada dado dentro da lista de dados
+    resultados += `
+    <div class="item-resultado">
+        <h2 class="link-racao">
+            <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+        </h2>
+            <p class="descrição">${dado.descricao}</p>
+    </div>
+`
+}
+
+section.innerHTML = resultados
+}
